bfdc72c31451c97b76cad2221c220de8dd38f9ec
Hussnain Saeed <mianhussnainsaeed@gmail.com>
2022-03-03 17:31:45 +0500

    Enable Cors

--- ShoppingAPI/Controllers/ProductController.cs
+++ ShoppingAPI/Controllers/ProductController.cs
@@ -7,17 +7,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using ShoppingAPI.Models;
 
 namespace ShoppingAPI.Controllers
 {
     // Yahan Sirf Admin aye
+    [EnableCors("*", "*", "*")]
     public class ProductController : ApiController
     {
         private ShoppingContext db = new ShoppingContext();
 
         [AuthorizeAdmin]
+
         public IHttpActionResult GetProducts(string keyword="",int skip=0,int take=10)
         {
             return Json(db.Products.Where(x=>string.IsNullOrEmpty(keyword) || x.Title.Contains(keyword)).OrderByDescending(x=>x.AddedOn).Skip(skip).Take(take).ToList());
